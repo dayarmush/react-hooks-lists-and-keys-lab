7256f979cb0c081ec5a8b62783d585d0616c5f81
dovid yarmush <yarmushda@gmail.com>
2023-05-22 17:49:13 -0400

    working project list

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,19 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+  
+let count = 1  
+
+const techArr = technologies.map(tech => {
+  return <span key={count++}>{tech}</span>
+})
+
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {techArr}
       </div>
     </div>
   );
