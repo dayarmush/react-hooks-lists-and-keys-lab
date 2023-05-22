7806bdf79b02b8ae657de6a9f3e6e3651e3dca7c
dovid yarmush <yarmushda@gmail.com>
2023-05-22 19:05:20 -0400

    complete react hooks and keys lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,7 @@ function NavBar() {
   const links = ["home", "about", "projects"];
 
   const navBar = links.map(link => {
-    return <a key={link} href={link} >{link}</a>
+    return <a key={link} href={`#${link}`} >{link}</a>
   })
 
   return <nav>{navBar}</nav>;
